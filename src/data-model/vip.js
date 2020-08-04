e6fa7b899f6f3e0b0901a606e5b10aad27249cb3
sali itzhaki <sali1943@hotmail.com>
2020-08-04 10:11:25 +0300

    tuning stuff

--- src/data-model/vip.js
+++ src/data-model/vip.js
@@ -9,9 +9,9 @@ state ={vip_ar:vip_arr}
            <div className="container">
                <div className="row">
                    <div class="rounded col-lg-6 border m-2">
-                     <img src="https://specials-images.forbesimg.com/imageserve/59d502f931358e542c034e76/200x200.jpg?background=000000&cropX1=245&cropX2=2420&cropY1=636&cropY2=2813" class="rounded float-left m-2"/>
-                    <h2>Name:</h2>
-                    <div>Company:</div>
+                     <img src="https://specials-images.forbesimg.com/imageserve/59d502f931358e542c034e76/200x200.jpg?background=000000&cropX1=245&cropX2=2420&cropY1=636&cropY2=2813" class="rounded-circle float-left m-2"/>
+                    <h2>Name: Bill Gates</h2>
+                    <div>Company: Microsoft</div>
                    </div>
                </div>
            </div>
