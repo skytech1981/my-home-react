7f6588db4266081dab5cc98a48cd7e8bd89981f3
sali itzhaki <sali1943@hotmail.com>
2020-08-04 12:10:33 +0300

    adjusting stuff

--- src/data-model/vip.js
+++ src/data-model/vip.js
@@ -8,13 +8,14 @@ state ={vip_ar:vip_arr}
         return (
                <div className="container bg-dark d-flex " >
                <div className="row d-flex justify-content-center">
-                   {this.state.vip_ar.map(item =>{return (<div class="rounded col-sm-5 border m-4">
+                   {this.state.vip_ar.map(item =>{return (
+                   <div class="rounded col-sm-5 border m-4">
                      <img src={item.image} class="w-50% img-fluid img-thumbnail rounded-circle float-left m-4 mt-4 ml-3"/>
                     <h2 className="bg-primary rounded mt-2">{item.name}</h2>
-                    <h4 className="bg-warning rounded">Company : {item.source}</h4>
+                    <h4 className="bg-light rounded">Company : {item.source}</h4>
                     <h5 className="bg-light rounded">Country:{item.country}</h5>
                     <h5 className="bg-light rounded">Birth Year:{item.birth_year}</h5>
-                    <h4 className="bg-success rounded">Value : {item.worth}</h4>
+                    <h4 className="bg-warning rounded">Value : {item.worth}</h4>
                    </div>) })}
                    
             
